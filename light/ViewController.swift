418df73dfda7a1bf0467a24eb91d022ce032e480
Alex <awest36@rocketmail.com>
2019-02-20 12:11:10 -0600

    update

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    var lightYellow = false
+    var lightGreen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateYellow()
+        updateGreen()
+        updateUI()
     }
-
+    @IBAction func yellowButton(_ sender: Any) {
+        lightYellow = !lightYellow
+        updateYellow()
+        
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    @IBAction func greenButton(_ sender: Any) {
+        lightGreen = !lightGreen
+        updateGreen()
+    }
+    func updateUI() {
+       view.backgroundColor = lightOn ? .white : .black
+      }
+    func updateYellow() {
+        view.backgroundColor = lightYellow ? .yellow : .black
+    }
+    func updateGreen()  {
+        view.backgroundColor = lightGreen ? .green : .black
+    }
 
 }
 
